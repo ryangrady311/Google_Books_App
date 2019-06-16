d9a2b86991660e24ed0dd1fee21ae84d650eeb8f
Ryan Grady <ryangrady250@gmail.com>
2019-06-16 14:22:42 -0700

    Updates

---
+++ precache-manifest.8176742daaf104792573697b7930405b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628caff0197debc6546b2e91899ae7ea",
+    "url": "/Google_Books_App/index.html"
+  },
+  {
+    "revision": "2eba5559c664abe2d84d",
+    "url": "/Google_Books_App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Google_Books_App/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2eba5559c664abe2d84d",
+    "url": "/Google_Books_App/static/js/main.8571f678.chunk.js"
+  },
+  {
+    "revision": "839b0136c99343540bdb",
+    "url": "/Google_Books_App/static/js/runtime~main.bdf28bbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Google_Books_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
